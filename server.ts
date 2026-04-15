4b283b7154b87860b32d2ca03422995dad3cd6b7
Nicholaus Costo Chetty <65787110+CostoVS@users.noreply.github.com>
2026-04-15 20:32:19 +0200

    Yyyyyyyyyy

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const { Pool } = pg;
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(cors());
   app.use(express.json());
